67dd2e43439e2f33427e0fad79f3606660d8a5e3
Yassine Cherif <cherif.esir@gmail.com>
2015-05-15 18:50:40 +0200

    cleanup

--- src/jchess/core/pieces/Piece.java
+++ src/jchess/core/pieces/Piece.java
@@ -15,14 +15,8 @@
 
 package jchess.core.pieces;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.awt.Point;
-import java.awt.RenderingHints;
-import java.awt.image.BufferedImage;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
